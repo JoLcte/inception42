ecb387c0752d958e6582972855c1a8e2d66891ff
JoLcte <lecomte.jo@hotmail.fr>
2022-10-08 17:27:29 +0200

    a bit of mariadb + beginning of wordpress)

---
+++ srcs/requirements/mariadb/tools/run.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+# Launching MariaDB server
+service mysql start;
+
+# Creating Database SQL_DATABASE
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# Creating user SQL_USER
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# Giving all privileges to SQL_USER
+mysql -e "GRANT ALL PRIVILEGES ON \'${SQL_DATABASE}\'.* TO \`{SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# Replacing root password by SQL_ROOT_PASSWORD
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+# Reloading caches used by MariaDB
+mysql -e "FLUSH PRIVILEGES;"
+
+# Shuting down MariaDB using root privileges
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+# Relaunching MariaDB
+exec mysqld_safe
