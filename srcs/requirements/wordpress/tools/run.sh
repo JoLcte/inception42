4a45eb02da5f44366a86da26d7d5aa7f16acd607
JoLcte <lecomte.jo@hotmail.fr>
2022-10-09 19:25:03 +0200

    docker compose in progress

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -8,5 +8,29 @@ if [! -e /var/www/wordpress/wp-config.php]; then
 	--dbname=$SQL_DATABASE \
 	--dbuser=$SQL_USER \
 	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	--dbhost=mariadb:3306 \
+	--path='/var/www/wordpress'
 
+wp core install --url=$DOMAIN_NAME \
+	 --title=$WP_TITLE \
+	--admin_user=$ADMIN_USER \
+	--admin-password=$ADMIN_PASSWORD \
+	--admin-email=$ADMIN_EMAIL \
+	--path='var/www/wordpress'
+
+wp user create --allow-root \
+	$USER_NAME \
+	$USER_EMAIL \
+	--role=author \
+	--user_pass=$USER_PASSWORD \
+	--path='var/www/wordpress' \
+	>> /log.txt
+fi
+
+# PHP error handle in case /run/php does not exist
+if [! -d /run/php ]; then
+	mkdir ./run/php
+fi
+
+# Launch php-fpm
+/usr/sbin/php-fpm7.3 -F
