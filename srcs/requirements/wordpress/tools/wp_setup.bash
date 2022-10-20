54886509458c6be1027608b7c943c779dc6efb57
JoLcte <lecomte.jo@hotmail.fr>
2022-10-20 19:06:33 +0200

    wordpress working with redis cache object installed but monitor different ?

--- srcs/requirements/wordpress/tools/wp_setup.bash
+++ srcs/requirements/wordpress/tools/wp_setup.bash
@@ -22,31 +22,37 @@ if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 	--dbhost=$SQL_HOST:$SQL_PORT \
 	--path='/var/www/html/wordpress'
 
-	sudo -u www-data wp core install
-	 --url=$DOMAIN_NAME \
-	 --title=$WP_TITLE \
+	sudo -u www-data wp core install \
+	--url=$DOMAIN_NAME \
+	--title=$WP_TITLE \
 	--admin_user=$ADMIN_USER \
 	--admin_password=$ADMIN_PASSWORD \
 	--admin_email=$ADMIN_EMAIL \
-	--path='var/www/html/wordpress'
+	--path='/var/www/html/wordpress'
 
 	sudo -u www-data wp user create \
 	$USER_NAME \
-	$USER_EMAIL \
+	$USER_MAIL \
 	--role=author \
 	--user_pass=$USER_PASSWORD \
 	--path='/var/www/html/wordpress'
 
 	# For Redis Bonus Service
-	sudo -u www-data wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME \
-	--path='/var/www/html/wordpress' \
-
+	cd /var/www/html/wordpress
+	sudo -u www-data wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME
+	sudo -u www-data wp config set WP_REDIS_HOST $REDIS_HOST
+	sudo -u www-data wp config set WP_REDIS_PORT $REDIS_PORT
+#	sudo -u www-data wp plugin install redis-cache --activate --path='/var/www/html/wordpress' -> already activated and installed
+#	sudo -u www-data wp plugin update --all --path='/var/www/html/wordpress' -> already updated
 fi
 
 # PHP error handle in case /run/php does not exist
 if [ ! -d /run/php ]; then
 	mkdir /run/php
 fi
+
+# Launch redis
+sudo -u www-data wp redis enable --path='/var/wwww/html/wordpress'
 
 # Launch php-fpm
 /usr/sbin/php-fpm7.3 -FR
