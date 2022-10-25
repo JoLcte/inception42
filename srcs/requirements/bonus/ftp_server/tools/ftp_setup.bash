88cc1067fdb1f2cb7c911d125b81cca4ee02244a
JoLcte <lecomte.jo@hotmail.fr>
2022-10-25 14:15:46 +0200

    vsftpd end setup

--- srcs/requirements/bonus/ftp_server/tools/ftp_setup.bash
+++ srcs/requirements/bonus/ftp_server/tools/ftp_setup.bash
@@ -1,25 +1,30 @@
 #! /bin/bash
 
-if [ ! =f "/etc/vsftpd/vsftpd.conf.backup" ]; then
+if [ ! -f "/etc/vsftpd.conf.backup" ]; then
 	
-	cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.backup
-	mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
+	cp /etc/vsftpd.conf /etc/vsftpd.conf.backup
+	mv /tmp/vsftpd.conf /etc/vsftpd.conf
 
-# Add user FTP_ADMIN
+# Add user FTP_ADMIN (not in jail for example but should be)
 
 	#create user w/o prompt for password
-	adduser $FTP_ADMIN --disabled-password
+	adduser $FTP_ADMIN --disabled-password --gecos ""
 	#change empty password to actual password
 	echo "$FTP_ADMIN:$FTP_ADMIN_PWD" | /usr/sbin/chpasswd
-	# change rights on wp directory for this user
-	echo $FTP_ADMIN | tee -a/etcvsftpd.userlist
+	# add FTP_ADMIN to the userlist
+	echo $FTP_ADMIN | tee -a /etc/vsftpd.userlist
 
 # ADD user FTP_USER and put it in jail
 	
-	adduser $FTP_USER --disabled-password
+	adduser $FTP_USER --disabled-password --gecos ""
 	echo "$FTP_USER:$FTP_USER_PWD" | /usr/sbin/chpasswd
-	echo $FTP_USER | tee -a/etcvsftpd.userlist
+	echo $FTP_USER | tee -a /etc/vsftpd.userlist
 	#put FTP_USER in chroot_list
-	echo $FTP_USER | tee -a/etcvsftpd.chroot_list
- # ... TO BE CONTINUED
+	echo $FTP_USER | tee -a /etc/vsftpd.chroot_list
+	
+# Make a directory for jail
+	mkdir -p /wp_ftp && chown $FTP_ADMIN /wp_ftp
+fi
 
+echo "FTP started on :21"
+/usr/sbin/vsftpd /etc/vsftpd.conf
